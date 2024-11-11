63435ed257523002242b99077027e7a357eca04a
RAVEL SIMBA <156199207+simbaravel23@users.noreply.github.com>
2024-11-11 08:03:06 -0300

    Atualizar o index.js cora e bady-parser

--- treino_rotas/index.js
+++ treino_rotas/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2/promise');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
